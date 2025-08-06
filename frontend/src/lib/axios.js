409ff3486d49f68751c46c71ef1d5603aeecfc31
ayushdwivedi12 <pd8814312@gmail.com>
2025-08-06 18:46:56 +0530

    fixed BASE_URL

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // In production there is no localhost , so we have to make this dynamic.
-BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api";
+const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api";
 const api = axios.create({
   baseURL: BASE_URL,
 });
