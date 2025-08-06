ddb1e5b79b9f32816fcbd6af66c4fb09c29b079b
ayushdwivedi12 <pd8814312@gmail.com>
2025-08-06 18:20:01 +0530

    ready for deployment

--- backend/src/app.js
+++ backend/src/app.js
@@ -7,16 +7,29 @@ const rateLimiter = require("./middlewares/ratelimiter");
 const app = express();
 const PORT = process.env.PORT || 5001;
 const cors = require("cors");
+const path = require("path");
 
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-  })
-);
+
+if (process.env.NODE_ENV !== "production") {
+  app.use(
+    cors({
+      origin: "http://localhost:5173",
+    })
+  );
+}
+
 app.use(express.json());
 app.use(rateLimiter);
 
 app.use("/api/notes", notesRoute);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../../frontend", "dist", "index.html"));
+  });
+}
 
 connectToMongoDB().then(() => {
   app.listen(PORT, () => {
