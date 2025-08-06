67587f7bf6ad92c848a7ba8495bb23d63bd8e58d
ayushdwivedi12 <pd8814312@gmail.com>
2025-08-06 18:58:22 +0530

    ratelimiter updated

--- backend/src/config/upstash.js
+++ backend/src/config/upstash.js
@@ -4,7 +4,7 @@ const { Redis } = require("@upstash/redis");
 
 const ratelimit = new Ratelimit({
   redis: Redis.fromEnv(),
-  limiter: Ratelimit.slidingWindow(20, "60 s"),
+  limiter: Ratelimit.slidingWindow(100, "60 s"),
 });
 
 module.exports = ratelimit;
